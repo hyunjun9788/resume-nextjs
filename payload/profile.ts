991e8ae1b2d65caf03057b6d6028d5ee8068f90f
hyunjun9788 <namhyunjun9788@gmail.com>
2024-10-26 18:21:57 +0900

    chore: 소개글 수정

--- payload/profile.ts
+++ payload/profile.ts
@@ -2,12 +2,13 @@ import { faEnvelope, faPen, faPhone } from '@fortawesome/free-solid-svg-icons';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 import { IProfile } from '../component/profile/IProfile';
+import image from '../asset/profile.jpg';
 
 const profile: IProfile.Payload = {
   disable: false,
 
   // image: 'https://resume.yowu.dev/static/image/profile_2019.png',
-  image: '',
+  image,
   name: {
     title: '남현준',
     small: '',
