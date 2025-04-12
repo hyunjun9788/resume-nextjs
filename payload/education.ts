79b8ba50ce730ae798526a71fe91a9b9f3035c77
hyunjun9788 <namhyunjun9788@gmail.com>
2025-04-13 01:31:51 +0900

    chore: 이력서 개선

--- payload/education.ts
+++ payload/education.ts
@@ -16,8 +16,8 @@ const education: IEducation.Payload = {
     {
       title: '왓에버 멘토링',
       subTitle: [
-        '- JavaScript 코어 지식을 학습하고, SPA를 직접 구현하며 SPA에 대한 이해도를 향상시켰습니다.',
-        '- JavaScript로 Virtual DOM을 구현하며 JSX의 트랜스파일 과정을 이해하고, 동작 원리를 학습했습니다.',
+        'JavaScript 코어 지식을 학습하고, SPA를 직접 구현하며 SPA에 대한 이해도를 향상시켰습니다.',
+        'JavaScript로 Virtual DOM을 구현하며 JSX의 트랜스파일 과정을 이해하고, 동작 원리를 학습했습니다.',
       ],
 
       startedAt: '2024-12',
@@ -26,8 +26,8 @@ const education: IEducation.Payload = {
     {
       title: '코드잇 스프린트 프론트엔드 엔지니어 부트캠프 수료',
       subTitle: [
-        '- GitHub를 통한 협업과 코드 리뷰 과정을 통해 협업 능력을 키웠으며, 프로젝트 배포까지 완료했습니다.',
-        '- 팀 프로젝트에서 Next.js를 활용해 SSR 적용해보며, CSR 차이를 경험하고 이해했습니다.',
+        'GitHub를 통한 협업과 코드 리뷰 과정을 통해 협업 능력을 키웠으며, 프로젝트 배포까지 완료했습니다.',
+        '팀 프로젝트에서 Next.js를 활용해 SSR 적용해보며, CSR 차이를 경험하고 이해했습니다.',
       ],
 
       startedAt: '2023-12',
@@ -35,10 +35,7 @@ const education: IEducation.Payload = {
     },
     {
       title: '충남대학교 정보통계학과 (편입)',
-      subTitle: [
-        '- 편입을 목표로 군 복무동안 전공 공부와 학점은행제를 병행하며, 꾸준한 노력 끝에 2년 동안 140학점을 이수하고 편입에 성공했습니다.',
-        '- R 프로그래밍을 활용한 데이터 분석 및 다양한 분석 기법에 대해 학습했습니다.',
-      ],
+      subTitle: [' R 프로그래밍을 활용한 데이터 분석 및 다양한 분석 기법에 대해 학습했습니다.'],
       startedAt: '2021-03',
       endedAt: '2023-08',
     },
