5a18a9186abee7abb5f311d4e44f4ca324d1afe4
hyunjun9788 <namhyunjun9788@gmail.com>
2025-04-13 02:46:09 +0900

    chore: 이력서 개선

--- component/common/CommonSection.tsx
+++ component/common/CommonSection.tsx
@@ -8,7 +8,7 @@ export function CommonSection({ title, children }: PropsWithChildren<{ title: st
       <EmptyRowCol>
         <Row className="pb-3">
           <Col>
-            <h2 style={{ color: '#3c78d8', marginTop: title === 'EDUCATION' ? '200px' : '30px' }}>
+            <h2 style={{ color: '#3c78d8', marginTop: title === 'EDUCATION' ? '230px' : '30px' }}>
               <span>{title}</span>
             </h2>
           </Col>
